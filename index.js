0b9da523260ffbd7eff29125852488c62f1c41d3
Kara Jacobs <codetato@outlook.com>
2024-04-19 10:20:29 +0200

    Updating js file content

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -21,6 +19,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
   renderGame()
 }
 
@@ -47,6 +50,5 @@ function newCard() {
   let card = getRandomCard()
   sum += card
   cards.push(card)
-  console.log(cards)
   renderGame()
 }
