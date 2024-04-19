3404359d671fb205efa749daec5c4289718f1a48
Kara Jacobs <codetato@outlook.com>
2024-04-19 08:11:31 +0200

    Adding js file and content

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 6
-let secondCard = 9
-
+let firstCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
-console.log(sum)
+function startGame() {
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game!"
+    isAlive = false
+} 
+  messageEl.textContent = message
+}
